21ceec41dc877fa342613f988e142d4a8b86c8b7
Bodiamost <bodiamost@gmail.com>
2017-02-09 05:07:33 +0300

    PatientsMVC
    My version of this assignment

---
+++ BohdanMVC/PatientsMVC/Models/PatientsContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace PatientsMVC.Models
+{
+    public class PatientsContext: DbContext
+    {
+        public DbSet<Patients> Patients { get; set; }
+    }
+}
