aa273cb5bf1c084b6d04c03c448169adab9c3c84
caelialuk <caelialuk@gmail.com>
2017-04-18 11:11:38 -0400

    Add FAQ Controller

---
+++ Mahc_Final/Mahc_Final/Controllers/faqsVisitorController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mahc_Final.DBContext;
+
+namespace Mahc_Final.Controllers
+{
+    public class faqsVisitorController : Controller
+    {
+        private HospitalContext db = new HospitalContext();
+
+        // GET: faqsVisitor
+        public ActionResult Index()
+        {
+            return View(db.faqs.ToList());
+        }
+
+        // GET: faqsVisitor/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            faq faq = db.faqs.Find(id);
+            if (faq == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faq);
+        }
+
+        // GET: faqsVisitor/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: faqsVisitor/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Question,Answer")] faq faq)
+        {
+            if (ModelState.IsValid)
+            {
+                db.faqs.Add(faq);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(faq);
+        }
+
+        // GET: faqsVisitor/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            faq faq = db.faqs.Find(id);
+            if (faq == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faq);
+        }
+
+        // POST: faqsVisitor/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Question,Answer")] faq faq)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(faq).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(faq);
+        }
+
+        // GET: faqsVisitor/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            faq faq = db.faqs.Find(id);
+            if (faq == null)
+            {
+                return HttpNotFound();
+            }
+            return View(faq);
+        }
+
+        // POST: faqsVisitor/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            faq faq = db.faqs.Find(id);
+            db.faqs.Remove(faq);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
