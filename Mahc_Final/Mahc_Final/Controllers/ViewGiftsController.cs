157b8285f71b7cc2df88fe988602f48e2e22c2b7
Sharanjeet Kaur <Sharanjeet Kaur>
2017-04-25 10:30:45 -0400

    Sharan's features added

---
+++ Mahc_Final/Mahc_Final/Controllers/ViewGiftsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Mahc_Final.Models;
+using Mahc_Final.DBContext;
+
+namespace Mahc_Final.Controllers
+{
+    public class ViewGiftsController : Controller
+    {
+        private HospitalContext db = new HospitalContext();
+
+        // GET: ViewGifts
+        public ActionResult Index()
+        {
+            var gifts = db.Gifts.Include(g => g.GiftCat);
+            return View(gifts.ToList());
+        }
+
+        // GET: ViewGifts/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gift gift = db.Gifts.Find(id);
+            if (gift == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gift);
+        }
+        /* Viewers can not Create or Delete a Gift they can just view the list or details about a particular gift
+        // GET: ViewGifts/Create
+        public ActionResult Create()
+        {
+            ViewBag.cat_id = new SelectList(db.GiftCats, "Id", "cat_name");
+            return View();
+        }
+
+        // POST: ViewGifts/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,cat_id,name,desc,image,price")] Gift gift)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Gifts.Add(gift);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.cat_id = new SelectList(db.GiftCats, "Id", "cat_name", gift.cat_id);
+            return View(gift);
+        }
+
+        // GET: ViewGifts/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gift gift = db.Gifts.Find(id);
+            if (gift == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.cat_id = new SelectList(db.GiftCats, "Id", "cat_name", gift.cat_id);
+            return View(gift);
+        }
+
+        // POST: ViewGifts/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,cat_id,name,desc,image,price")] Gift gift)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gift).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.cat_id = new SelectList(db.GiftCats, "Id", "cat_name", gift.cat_id);
+            return View(gift);
+        }
+
+        // GET: ViewGifts/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gift gift = db.Gifts.Find(id);
+            if (gift == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gift);
+        }
+
+        // POST: ViewGifts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Gift gift = db.Gifts.Find(id);
+            db.Gifts.Remove(gift);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        */
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
