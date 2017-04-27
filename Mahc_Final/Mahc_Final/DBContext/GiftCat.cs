0ac048e39b236d5f7822a20101ad1286f3a4d733
sharan78620 <sharan78620@yahoo.com>
2017-04-27 16:16:07 -0400

    Update GiftCat.cs
    Added Data annotations

--- Mahc_Final/Mahc_Final/DBContext/GiftCat.cs
+++ Mahc_Final/Mahc_Final/DBContext/GiftCat.cs
@@ -11,6 +11,7 @@ namespace Mahc_Final.DBContext
 {
     using System;
     using System.Collections.Generic;
+     using System.ComponentModel.DataAnnotations;
     
     public partial class GiftCat
     {
@@ -21,6 +22,9 @@ namespace Mahc_Final.DBContext
         }
     
         public int Id { get; set; }
+         [Required]
+        [StringLength(255, ErrorMessage = "Category must be at least 2 characters", MinimumLength = 2)]
+        [Display(Name = "Gift Category")]
         public string cat_name { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
