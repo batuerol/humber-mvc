0329304dc76efbdaebe8442505c377de03cb7bc8
sharan78620 <sharan78620@yahoo.com>
2017-05-17 11:59:25 -0400

    Added Partial view and missing class

---
+++ Mahc_Final/Mahc_Final/DBContext/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Mahc_Final.DBContext;
+
+namespace Mahc_Final.Models
+{
+    public class UserInfo
+    {
+
+        public string CardNumber { get; set; }
+        public string ExpYear { get; set; }
+        public string ExpMonth { get; set; }
+        public string Cvc { get; set; }
+    }
+}
